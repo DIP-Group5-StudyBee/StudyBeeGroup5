e32c113bba99d49168c7860fce593459a39f44b2
jack0025 <jack0025@e.ntu.edu.sg>
2020-10-21 17:33:41 +0800

    done display friend and basic details

--- app/src/main/java/com/example/studybee/basicDetails.java
+++ app/src/main/java/com/example/studybee/basicDetails.java
@@ -4,13 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONObject;
 import org.json.JSONStringer;
 
-public class basicDetails extends AppCompatActivity{
+public class basicDetails extends AppCompatActivity implements OnTaskCompleted{
 
     TextView tname;
     TextView tfaculty;
@@ -18,9 +19,12 @@ public class basicDetails extends AppCompatActivity{
     String msgType;
     String username, sfacu, sname,smail;
     String status;
+    String myuser = "lsy";// hard coded need to retrieve information from user profile
+    Button btnfriend;
+    int id1;
 
     // Set host address of the WAMP Server
-    public static final String HOST = "192.168.1.106"; //use your IP address
+    public static final String HOST = "10.27.208.223"; //use your IP address
 
     // Set virtual directory of the host website
     public static final String DIR = "myproject";
@@ -49,7 +53,59 @@ public class basicDetails extends AppCompatActivity{
 
         }
     public void SButtonOnClickHandler(View v){
-        Toast.makeText(getApplicationContext(), "Successfully Added.", Toast.LENGTH_LONG).show();
+        String jsonString = convertToJSON();
+        //Toast.makeText(getApplicationContext(), "Successfully Added.", Toast.LENGTH_LONG).show();
+        //access database network
+        HttpAsyncTaskForLogin task = new HttpAsyncTaskForLogin(this);
+        task.execute("http://" + HOST + "/" + DIR + "/addFriend.php", jsonString);
     }
 
-}
+    private String convertToJSON() {
+        JSONStringer jsonText = new JSONStringer();
+        try {
+
+            jsonText.object();
+            jsonText.key("type");
+            jsonText.value(msgType);
+            jsonText.key("user1");
+            jsonText.value(myuser);
+            jsonText.key("user2");
+            jsonText.value(username);
+            jsonText.endObject();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return jsonText.toString();
+    }
+    public void retrieveFromJSON(String message) {
+        try {
+            JSONObject jsonObject = new JSONObject(message);
+            msgType = jsonObject.getString("type");
+            if (msgType.equals(REQ_DOWNLOAD)) {
+                status = jsonObject.getString("status");
+                if (status.equals("OK")) {
+                    id1 = jsonObject.getInt("id1");
+                    myuser= jsonObject.getString("user1");
+                    username = jsonObject.getString("user2");
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onTaskCompleted(String response) {
+        retrieveFromJSON(response);
+        btnfriend = findViewById(R.id.button13);
+        //if ((msgType.equals(REQ_DOWNLOAD))  status.equals("OK")) {
+        btnfriend.setVisibility(View.INVISIBLE);
+        Toast.makeText(getApplicationContext(), "Successfully Added.", Toast.LENGTH_LONG).show();
+        // }
+        //else {
+        // Toast.makeText(getApplicationContext(), "Fail", Toast.LENGTH_SHORT).show();
+        return;
+        //}
+    }}
