12b56e9a5c4a5194296c2f33f1228ae552edd3f4
Songyu-winter <slyu003@e.ntu.edu.sg>
2020-12-07 18:10:25 +0800

    change for home page upcoming meeting (change hardcode to real function)

--- app/src/main/java/com/example/studybee/HomeFragment.java
+++ app/src/main/java/com/example/studybee/HomeFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
@@ -15,19 +16,55 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.studybee.adapter.UpcomingMeetingAdapter;
 import com.example.studybee.model.UpcomingMeeting;
 
+import org.json.JSONObject;
+import org.json.JSONStringer;
+
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.TimeZone;
 
 import static android.content.Context.MODE_PRIVATE;
+import static com.example.studybee.initsdk.AuthConstants.ip;
 
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements OnTaskCompleted {
 
     private RecyclerView upcomingMeetingRv;
     private UpcomingMeetingAdapter upcomingMeetingAdapter;
     private CardView createButton, joinButton;
     private String isTA;
 
+    String host_name;
+    int user_id;
+    String[] Meet_str =new String[3];
+    String[] StartTime_str= new String[3];
+
+    String time;
+    int numMeet = 0;
+
+    String status;
+    String msgType;
+
+
+    String Username;
+    TextView Meet1;
+    TextView Meet2;
+    TextView Meet3;
+    TextView StartTime1;
+    TextView StartTime2;
+    TextView StartTime3;
+
+    private final String TAG = this.getClass().getSimpleName();
+
+    // Set host address of the WAMP Server
+    public static final String HOST = ip; //using your own IP address
+
+    // Set virtual directory of the host website
+    public static final String DIR = "myproject";
+
+    // Set request ID for all HTTP requests
+    private static final String REQ_DOWNLOAD = "1004";
 
     public HomeFragment() {
         // Required empty public constructor
@@ -42,14 +79,36 @@ public class HomeFragment extends Fragment {
 
         upcomingMeetingRv = rootView.findViewById(R.id.upcoming_meetings_rv);
 
+//        Username =  (TextView) findViewById(R.id.Username_record);
+        SharedPreferences sh = this.getActivity().getSharedPreferences("preference", MODE_PRIVATE);
+//        Username = sh.getString("username", "");
+        host_name = sh.getString("username","");
+        user_id = sh.getInt("id",0);
+
+        TimeZone tz = TimeZone.getTimeZone("GMT+8");
+        Calendar c = Calendar.getInstance(tz);
+        time =String.format("%04d" , c.get(Calendar.YEAR))+"-"+
+                String.format("%02d" , c.get(Calendar.MONTH)+1)+"-"+
+                String.format("%02d" , c.get(Calendar.DAY_OF_MONTH))+" "+
+                String.format("%02d" , c.get(Calendar.HOUR_OF_DAY))+":"+
+                String.format("%02d" , c.get(Calendar.MINUTE))+":"+
+                String.format("%02d" , c.get(Calendar.SECOND));
+
+        msgType = REQ_DOWNLOAD;
+
+        String jsonString = convertToJSON();
+        HttpAsyncTaskForLogin task = new HttpAsyncTaskForLogin(this);
+        task.execute("http://"+HOST+"/"+DIR+"/retrieveUMeet.php",
+                jsonString);
+
         //set up dummy data for model class for upcoming meetings
-        List<UpcomingMeeting> upcomingMeetingList = new ArrayList<>();
-
-        upcomingMeetingList.add(new UpcomingMeeting("Title 1", "Java Programming", "12hrs"));
-        upcomingMeetingList.add(new UpcomingMeeting("Title 2", "Engine Comm", "12hrs"));
-        upcomingMeetingList.add(new UpcomingMeeting("Title 3", "Alphabets", "12hrs"));
-
-        setUpcomingRecycler(upcomingMeetingList);
+//        List<UpcomingMeeting> upcomingMeetingList = new ArrayList<>();
+//
+//        upcomingMeetingList.add(new UpcomingMeeting("Title 1", "Java Programming", "12hrs"));
+//        upcomingMeetingList.add(new UpcomingMeeting("Title 2", "Engine Comm", "12hrs"));
+//        upcomingMeetingList.add(new UpcomingMeeting("Title 3", "Alphabets", "12hrs"));
+//
+//        setUpcomingRecycler(upcomingMeetingList);
 
         createButton = rootView.findViewById(R.id.createButton);
         joinButton = rootView.findViewById(R.id.joinButton);
@@ -82,6 +141,93 @@ public class HomeFragment extends Fragment {
         });
 
         return rootView;
+    }
+
+    public String convertToJSON() {
+        JSONStringer jsonText = new JSONStringer();
+        try {
+            jsonText.object();
+            jsonText.key("type");
+            jsonText.value(msgType);
+            jsonText.key("start_time");
+            jsonText.value(time);
+            jsonText.key("user_id");
+            jsonText.value(user_id);
+            jsonText.endObject();
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return jsonText.toString();
+    }
+
+    public void retrieveFromJSON(String message) {
+        try {
+            JSONObject jsonObject = new JSONObject(message);
+            msgType = jsonObject.getString("type");
+            if (msgType.equals(REQ_DOWNLOAD)){
+                status = jsonObject.getString("status");
+                if(status.equals("OK")){
+
+                    //task option will retrieve the correct only taking 3 values
+                    numMeet = jsonObject.getInt("number");
+                    for (int i = 0; i<numMeet; i++) {
+                        Meet_str[i] = jsonObject.getString("meeting_name"+i);
+                        StartTime_str[i] = jsonObject.getString("meeting_time"+i);
+                    }
+                    for (int i = numMeet +1; i<3; i++){
+                        //not value return
+                        Meet_str[i] = null;
+                        StartTime_str[i] = null;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onTaskCompleted(String response) {
+        // retrieve response information from JSON
+        retrieveFromJSON(response);
+
+        List<UpcomingMeeting> upcomingMeetingList = new ArrayList<>();
+
+        setUpcomingRecycler(upcomingMeetingList);
+
+        if(Meet_str[0]!= null){
+
+            upcomingMeetingList.add(new UpcomingMeeting("Title 1", Meet_str[0], StartTime_str[0]));
+//            Meet1.setText(Meet_str[0]);
+//            StartTime1.setText(StartTime_str[0]);
+        }
+        else{
+            upcomingMeetingList.add(new UpcomingMeeting("No meeting", "", ""));
+//            Meet1.setText("No Meeting Record");
+//            StartTime1.setText("");
+        }
+        if(Meet_str[1]!= null){
+            upcomingMeetingList.add(new UpcomingMeeting("Title 1", Meet_str[1], StartTime_str[1]));
+//            Meet2.setText(Meet_str[1]);
+//            StartTime2.setText(StartTime_str[1]);
+        }
+        else{
+            upcomingMeetingList.add(new UpcomingMeeting("No meeting", "", ""));
+//            Meet2.setText("No Meeting Record");
+//            StartTime2.setText("");
+        }
+        if(Meet_str[2]!= null){
+            upcomingMeetingList.add(new UpcomingMeeting("Title 1", Meet_str[2], StartTime_str[2]));
+//            Meet3.setText(Meet_str[2]);
+//            StartTime3.setText(StartTime_str[2]);
+        }
+        else{
+            upcomingMeetingList.add(new UpcomingMeeting("No meeting", "", ""));
+//            Meet3.setText("No Meeting Record");
+//            StartTime3.setText("");
+        }
+
     }
 
     //Method to attach the MeetingList with RecyclerView with dummy data
